ecba7c37e1846db1c3457ef5815e321714cfd68e
rickyli888 <rickyli860@gmail.com>
2018-02-03 23:10:24 -0800

    Third Commit

--- Cinema View/NowPlayingViewController.swift
+++ Cinema View/NowPlayingViewController.swift
@@ -12,14 +12,41 @@ class NowPlayingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            //Runs when network request returns
+            
+            if let error = error{
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                print (dataDictionary)
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies{
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+            
+        }
+        
+        task.resume()
     }
+    
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
